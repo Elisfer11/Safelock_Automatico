11a697d0dcc4c4fa5ac36b14cfd655a2b4c69e43
Elisfer11 <odroid@odroid.GALAWAY>
2023-11-03 09:13:26 -0600

    first commit

---
+++ Automatizacion_pi-hole.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+xdotool key Super_L+t
+sleep 4
+xdotool type "sudo curl -sSL https://raw.githubusercontent.com/pi-hole/pi-hole/master/automated%20install/basic-install.sh | bash"
+xdotool key Return
+sleep 1 
+xdotool type "odroid"
+xdotool key Return
+sleep 600
+xdotool key Return
+sleep 1
+xdotool key Return
+sleep 1
+xdotool key Left
+sleep 1
+xdotool key Return
+sleep 1
+xdotool key Return
+sleep 1
+xdotool key Return
+sleep 1
+xdotool key Return
+sleep 1
+xdotool key Return
+sleep 1
+xdotool key Return
+sleep 1
+xdotool key Return
+sleep 300
+xdotool key Return
+sleep 5
+xdotool type "exit"
+xdotool key Return
+
