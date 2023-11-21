32b3851b43c7a97443b9c24b6e45d677fd4becb4
Elisfer11 <elisfer11@outlook.com>
2023-11-21 10:07:52 -0600

    Ajuste en los permisos

--- Descargas_Semi_Manuales.py
+++ Descargas_Semi_Manuales.py
@@ -1,34 +1,27 @@
 import os
 import subprocess
 
-# Verifica si el usuario tiene permisos de superusuario
-if os.geteuid() != 0:
-    print("Este script debe ejecutarse como superusuario (root).")
-    exit(1)
-else:
-    # Actualiza el sistema
-    os.system("sudo apt update")
+#Update del sistema
+os.system("sudo apt-get update")
 
-    # Descarga e instala Pi-hole
-    os.system("sudo curl -sSL https://raw.githubusercontent.com/OpenLock20/SL-MASTER-PRODUCT/master/automated%20install/basic-install.sh | bash")
-    #cambiar contra
-    new_password = "safelock"
-    os.system(f'echo "{new_password}\n{new_password}" | sudo pihole -a -p')
+#Instalar Pi-hole
+os.system("sudo curl -sSL https://raw.githubusercontent.com/OpenLock20/SL-MASTER-PRODUCT/master/automated%20install/basic-install.sh | bash")
+new_password = "safelock"
+os.system(f'echo "{new_password}\n{new_password}" | sudo pihole -a -p')
 
-    # Cambiar al directorio /var/www/html y realizar operaciones
-    os.system("sudo rm -r /var/www/html/admin/")
-    os.system("sudo git clone --depth 1 https://github.com/OpenLock20/SL-INTERFACE-PRODUCT /var/www/html/admin")
+os.system("sudo rm -r /var/www/html/admin/")
+os.system("sudo git clone --depth 1 https://github.com/OpenLock20/SL-INTERFACE-PRODUCT /var/www/html/admin")
 
-    # Instala Yersinia
-    os.system("sudo apt install -y yersinia")
+#Instala Yersinia
+os.system("sudo apt install -y yersinia")
 
-    print("Pi-hole y Yersinia se han instalado correctamente.")
+#Instala dependencias para el envio de correos
+os.system("sudo apt-get install python3-pip")
+
+os.system("pip3 install requests")
 
 
-
-print("Configuración de Pi-hole y safelock completada, y el repositorio git ha sido clonado.")
-
-#Configura la tarea cron para ejecutar el script cada minuto en el crontab de root
+#Configura el crontab
 cronjob = '* * * * * python3 /var/www/html/admin/new_crontab.py\n'
 with open('/tmp/cronjob', 'w') as cronfile:
     cronfile.write(cronjob)
@@ -37,10 +30,11 @@ print("Tareas configuradas.")
 
 
 
-#Instalacion TeamViewer
-
+#Quita la suspencion automatica
 os.system("gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0")
 
+
+#Instalacion TeamViewer
 
 user = os.getlogin()
 
