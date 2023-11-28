52e8adfa5d1bbe19ec79c2c34e1bd0fb3ecabe3e
Elisfer11 <148561173+Elisfer11@users.noreply.github.com>
2023-11-28 10:28:41 -0600

    Actualizar Script_Automatizacion.py

--- Script_Automatizacion.py
+++ Script_Automatizacion.py
@@ -67,7 +67,7 @@ remove_wayland = f"WaylandEnable=false"
 with open('/etc/gdm3/custom.conf', 'a') as host_file:
     host_file.write(remove_wayland)
 
-time.sleep(15)
+time.sleep(30)
 
 os.system("systemctl restart gdm")
 
