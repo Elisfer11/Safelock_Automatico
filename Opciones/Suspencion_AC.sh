82296b56dc380f59328e89f12affd767d12e69e8
root <root@safelock.GALAWAY>
2023-11-28 09:41:58 -0600

    Se optimiza el Repositorio

--- Opciones/Suspencion_AC.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Desactivar la suspensión en AC
-gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 0
-
-echo "La suspensión en AC ha sido desactivada."
-
